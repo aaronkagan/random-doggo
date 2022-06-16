f9a3162db7c140e08d498aecee02fa3549284f06
Aaron Kagan <aaronkagan@yahoo.com>
2022-06-16 11:40:28 -0400

    adding fetch and redering. adding some CSS

--- src/App.js
+++ src/App.js
@@ -1,7 +1,35 @@
 import "./assets/stylesheets/App.css";
+import Button from "@mui/material/Button";
+import Card from "@mui/material/Card";
+import { useEffect, useState } from "react";
 
 function App() {
-  return <div className="App"></div>;
+  const [doggo, setDoggo] = useState();
+
+  const fetchData = () => {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => {
+        return response.json();
+      })
+      .then((data) => {
+        setDoggo(data["message"]);
+      });
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  return (
+    <div className="App">
+      <Card variant="outlined">
+        <img src={doggo}></img>
+        <Button variant="contained" onClick={fetchData}>
+          Next Doggo
+        </Button>
+      </Card>
+    </div>
+  );
 }
 
 export default App;
