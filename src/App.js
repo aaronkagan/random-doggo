417aeed175a2c5dbadf7e0285326755edd0af5d9
Aaron Kagan <aaronkagan@yahoo.com>
2022-06-16 14:23:29 -0400

    added doggo fact

--- src/App.js
+++ src/App.js
@@ -1,33 +1,84 @@
 import "./assets/stylesheets/App.css";
+import { useEffect, useState } from "react";
 import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
-import { useEffect, useState } from "react";
+import Typography from "@mui/material/Typography";
+import Skeleton from "@mui/material/Skeleton";
+import { CardContent, CardMedia } from "@mui/material";
 
 function App() {
-  const [doggo, setDoggo] = useState();
+  const [doggo, setDoggo] = useState(null);
+  const [doggoFact, setDoggoFact] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const fetchData = () => {
+  const fetchDoggoPic = () => {
     fetch("https://dog.ceo/api/breeds/image/random")
       .then((response) => {
         return response.json();
       })
       .then((data) => {
         setDoggo(data["message"]);
+        setIsLoading(true);
+      });
+  };
+
+  const fetchDoggoFact = () => {
+    fetch("https://dog-api.kinduff.com/api/facts")
+      .then((response) => response.json())
+      .then((data) => {
+        setDoggoFact(data["facts"][0]);
       });
   };
 
   useEffect(() => {
-    fetchData();
+    fetchDoggoPic();
+    fetchDoggoFact();
   }, []);
 
   return (
     <div className="App">
-      <Card sx={{"width": "90vw", "display": "flex", "flexDirection" : "column", "alignItems" : "center", "paddingTop" : "2rem", "paddingBottom" : "2rem"}}>
-        <img src={doggo}/>
+      <Card
+        sx={{
+          width: "90vw",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          paddingTop: "2rem",
+          paddingBottom: "2rem",
+        }}
+      >
+        {isLoading ? (
+          <CardMedia
+            component="img"
+            alt="doggo"
+            image={doggo}
+            height="300"
+            sx={{ width: "300px", objectFit: "cover" }}
+          />
+        ) : (
+          <Skeleton
+            variant="rectangle"
+            animation="wave"
+            height={300}
+            width={300}
+          />
+        )}
+        <CardContent sx={{ maxWidth: "300px" }}>
+          <Typography align="center" variant="body1">
+            {JSON.stringify(doggoFact)}
+          </Typography>
+        </CardContent>
       </Card>
-      <Button sx={{"marginTop": "3vh"}} variant="contained" onClick={fetchData}>
-          Next Doggo
-        </Button>
+      <Button
+        sx={{ marginTop: "3vh" }}
+        variant="contained"
+        onClick={() => {
+          fetchDoggoPic();
+          fetchDoggoFact();
+        }}
+      >
+        Next Doggo
+      </Button>
     </div>
   );
 }
