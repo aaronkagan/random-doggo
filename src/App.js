b7b9f664ff347303b6d7e1f96cf3133e2b7d7b6f
Aaron Kagan <aaronkagan@yahoo.com>
2022-06-16 16:54:04 -0400

    added css

--- src/App.js
+++ src/App.js
@@ -4,7 +4,8 @@ import Button from "@mui/material/Button";
 import Card from "@mui/material/Card";
 import Typography from "@mui/material/Typography";
 import Skeleton from "@mui/material/Skeleton";
-import { CardContent, CardMedia } from "@mui/material";
+import { AppBar, CardContent, CardMedia } from "@mui/material";
+import Container from "@mui/material/Container";
 
 function App() {
   const [doggo, setDoggo] = useState(null);
@@ -37,48 +38,65 @@ function App() {
 
   return (
     <div className="App">
-      <Card
+      <Container
         sx={{
-          width: "90vw",
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
-          paddingTop: "2rem",
-          paddingBottom: "2rem",
         }}
       >
-        {isLoading ? (
-          <CardMedia
-            component="img"
-            alt="doggo"
-            image={doggo}
-            height="300"
-            sx={{ width: "300px", objectFit: "cover" }}
-          />
-        ) : (
-          <Skeleton
-            variant="rectangle"
-            animation="wave"
-            height={300}
-            width={300}
-          />
-        )}
-        <CardContent sx={{ maxWidth: "300px" }}>
-          <Typography align="center" variant="body1">
-            {JSON.stringify(doggoFact)}
-          </Typography>
-        </CardContent>
-      </Card>
-      <Button
-        sx={{ marginTop: "3vh" }}
-        variant="contained"
-        onClick={() => {
-          fetchDoggoPic();
-          fetchDoggoFact();
-        }}
-      >
-        Next Doggo
-      </Button>
+        <Typography align="center" variant="h5" gutterBottom={true}>
+          Doggo Randomizer
+        </Typography>
+
+        <Card
+          sx={{
+            width: "90vw",
+            maxWidth: "500px",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            paddingTop: "2rem",
+            paddingBottom: "2rem",
+          }}
+        >
+          {isLoading ? (
+            <CardMedia
+              component="img"
+              alt="doggo"
+              image={doggo}
+              height="300"
+              sx={{ width: "300px", objectFit: "cover" }}
+            />
+          ) : (
+            <Skeleton
+              variant="rectangle"
+              animation="wave"
+              height={300}
+              width={300}
+            />
+          )}
+          <CardContent sx={{ maxWidth: "300px" }}>
+            <Typography align="center" variant="h5" gutterBottom={true}>
+              Fun Doggo Fact
+            </Typography>
+            <Typography align="center" variant="body1">
+              {JSON.stringify(doggoFact)}
+            </Typography>
+          </CardContent>
+        </Card>
+        <Button
+          sx={{ marginTop: "3vh" }}
+          variant="contained"
+          onClick={() => {
+            fetchDoggoPic();
+            fetchDoggoFact();
+          }}
+        >
+          Next Doggo
+        </Button>
+      </Container>
     </div>
   );
 }
